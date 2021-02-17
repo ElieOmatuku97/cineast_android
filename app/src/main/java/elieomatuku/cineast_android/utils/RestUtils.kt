6e66bf4581f7f8c9adce64c5f7ba39b2eaeedb06
Elie Omatuku <eomatuku@yahoo.fr>
2021-02-17 21:44:55 +0200

    Display Tmdb username

--- app/src/main/java/elieomatuku/cineast_android/utils/RestUtils.kt
+++ app/src/main/java/elieomatuku/cineast_android/utils/RestUtils.kt
@@ -8,6 +8,7 @@ object RestUtils {
     const val REQUEST_TOKEN_KEY = "request_token_key"
     const val SESSION_ID_KEY = "session_id_key"
     const val ACCOUNT_ID_KEY = "account_id_key"
+    const val ACCOUNT_USERNAME = "account_username_key"
 
     val API_KEY: String by lazy {
         BuildConfig.api_key
