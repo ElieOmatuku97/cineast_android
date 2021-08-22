de7dddb37f92edf689e45cabfafa20cfc31bdd18
Elie Omatuku <elieomatuku@gmail.com>
2021-08-22 15:42:46 +0200

    People usecases

---
+++ domain/src/main/java/elieomatuku/cineast_android/domain/interactor/people/GetDetails.kt
@@ -0,0 +1,27 @@
+package elieomatuku.cineast_android.domain.interactor.people
+
+import elieomatuku.cineast_android.domain.interactor.CompleteResult
+import elieomatuku.cineast_android.domain.interactor.UseCase
+import elieomatuku.cineast_android.domain.interactor.safeUseCaseCall
+import elieomatuku.cineast_android.domain.model.Person
+import elieomatuku.cineast_android.domain.model.PersonDetails
+import elieomatuku.cineast_android.domain.repository.PersonRepository
+
+
+/**
+ * Created by elieomatuku on 2021-08-22
+ */
+
+class GetDetails(private val personRepository: PersonRepository) :
+    UseCase<GetDetails.Input, CompleteResult<PersonDetails>> {
+
+    override suspend fun execute(params: Input): CompleteResult<PersonDetails> {
+        return safeUseCaseCall {
+            return@safeUseCaseCall personRepository.getDetails(
+                params.person
+            )
+        }
+    }
+
+    data class Input(val person: Person)
+}
---
+++ domain/src/main/java/elieomatuku/cineast_android/domain/interactor/people/GetMovies.kt
@@ -0,0 +1,27 @@
+package elieomatuku.cineast_android.domain.interactor.people
+
+import elieomatuku.cineast_android.domain.interactor.CompleteResult
+import elieomatuku.cineast_android.domain.interactor.UseCase
+import elieomatuku.cineast_android.domain.interactor.safeUseCaseCall
+import elieomatuku.cineast_android.domain.model.Movie
+import elieomatuku.cineast_android.domain.model.Person
+import elieomatuku.cineast_android.domain.repository.PersonRepository
+
+
+/**
+ * Created by elieomatuku on 2021-08-22
+ */
+
+class GetMovies(private val personRepository: PersonRepository) :
+    UseCase<GetMovies.Input, CompleteResult<List<Movie>>> {
+
+    override suspend fun execute(params: Input): CompleteResult<List<Movie>> {
+        return safeUseCaseCall {
+            return@safeUseCaseCall personRepository.getMovies(
+                params.person
+            )
+        }
+    }
+
+    data class Input(val person: Person)
+}
