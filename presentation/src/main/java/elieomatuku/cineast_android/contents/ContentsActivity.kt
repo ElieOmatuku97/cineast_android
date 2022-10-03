24bb0d2a29d473377a8774c56aa61fb63733533b
Elie Omatuku <elieomatuku@gmail.com>
2022-10-03 13:33:22 +0200

    ContentsActivity.kt migration to Jetpack Compose

--- presentation/src/main/java/elieomatuku/cineast_android/contents/ContentsActivity.kt
+++ presentation/src/main/java/elieomatuku/cineast_android/contents/ContentsActivity.kt
@@ -3,19 +3,19 @@ package elieomatuku.cineast_android.contents
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
 import elieomatuku.cineast_android.R
 import elieomatuku.cineast_android.domain.model.Content
 import elieomatuku.cineast_android.domain.model.Movie
 import elieomatuku.cineast_android.domain.model.Person
 import elieomatuku.cineast_android.base.BaseActivity
+import elieomatuku.cineast_android.databinding.ActivityContentBinding
 import elieomatuku.cineast_android.details.movie.MovieFragment
 import elieomatuku.cineast_android.details.person.PersonFragment
 import elieomatuku.cineast_android.utils.Constants
-import io.reactivex.Observable
-import io.reactivex.subjects.PublishSubject
-import kotlinx.android.synthetic.main.activity_content.*
+import elieomatuku.cineast_android.viewholder.MovieItem
 import java.io.Serializable
 
 class ContentsActivity : BaseActivity() {
@@ -42,57 +42,19 @@ class ContentsActivity : BaseActivity() {
         }
     }
 
-    private val contentSelectPublisher: PublishSubject<Content> by lazy {
-        PublishSubject.create<Content>()
-    }
-
-    private val contentSelectObservable: Observable<Content>
-        get() = contentSelectPublisher.hide()
-
-    lateinit var contentsAdapter: ContentsAdapter
-
-    private val moviesAdapter: ContentsAdapter by lazy {
-        ContentsAdapter(contentSelectPublisher)
-    }
-
-    private val peopleAdapter: ContentsAdapter by lazy {
-        ContentsAdapter(contentSelectPublisher)
-    }
-
-    private val listView: RecyclerView by lazy {
-        list_view_container
-    }
+    private lateinit var binding: ActivityContentBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_content)
+        binding = ActivityContentBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         val contents: List<Content> = intent.getSerializableExtra(WIDGET_KEY) as List<Content>
         val screenNameRes = intent.getIntExtra(Constants.SCREEN_NAME_KEY, 0)
 
         updateView(contents, screenNameRes)
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        rxSubs.add(
-            contentSelectObservable
-                .subscribe { content: Content ->
-                    val params = Bundle()
-
-                    params.putString(Constants.SCREEN_NAME_KEY, SCREEN_NAME)
-
-                    if (content is Person) {
-                        params.putSerializable(PEOPLE_KEY, content)
-                        gotoContent(params, PersonFragment::class.java)
-                    } else {
-                        params.putSerializable(MOVIE_KEY, content)
-                        gotoContent(params, MovieFragment::class.java)
-                    }
-                }
-        )
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -108,27 +70,51 @@ class ContentsActivity : BaseActivity() {
     }
 
     private fun setToolbarTitle(screenNameRes: Int? = null) {
-        toolbar?.title = screenNameRes?.let {
+        binding.toolbar.title = screenNameRes?.let {
             resources.getString(it)
         } ?: resources.getString(R.string.nav_title_discover)
     }
 
     private fun setContents(contents: List<Content>) {
-        contentsAdapter = if (areWidgetsMovies(contents)) moviesAdapter else peopleAdapter
-        contentsAdapter.contents = contents.toMutableList()
-        listView.layoutManager = LinearLayoutManager(this)
-        listView.adapter = contentsAdapter
-        contentsAdapter.notifyDataSetChanged()
-    }
+        binding.composeView.setContent {
+            ContentScreen(contents = contents) { content ->
+                val params = Bundle()
 
-    private fun areWidgetsMovies(contents: List<Content>): Boolean {
-        val firstElement: Content = contents.first()
-        return firstElement is Movie
+                params.putString(Constants.SCREEN_NAME_KEY, SCREEN_NAME)
+
+                if (content is Person) {
+                    params.putSerializable(PEOPLE_KEY, content)
+                    gotoContent(params, PersonFragment::class.java)
+                } else {
+                    params.putSerializable(MOVIE_KEY, content)
+                    gotoContent(params, MovieFragment::class.java)
+                }
+            }
+        }
     }
 
     private fun gotoContent(params: Bundle, contentActivityClass: Class<*>) {
         val intent = Intent(this, contentActivityClass)
         intent.putExtras(params)
         startActivity(intent)
+    }
+}
+
+@Composable
+fun ContentScreen(contents: List<Content>, onContentClick: (content: Content) -> Unit) {
+    LazyColumn {
+        items(contents) { content ->
+            when (content) {
+                is Person -> {
+                    elieomatuku.cineast_android.viewholder.ContentItem(
+                        imagePath = content.profilePath,
+                        title = content.name
+                    )
+                }
+                is Movie -> {
+                    MovieItem(movie = content)
+                }
+            }
+        }
     }
 }
