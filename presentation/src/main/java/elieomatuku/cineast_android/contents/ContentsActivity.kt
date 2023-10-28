44acafa9948094fc0636928a5821d7fbadaea7b2
Elie Omatuku <elieomatuku@gmail.com>
2023-10-29 01:47:52 +0200

    Fix ContentsActivity back button icon

--- presentation/src/main/java/elieomatuku/cineast_android/contents/ContentsActivity.kt
+++ presentation/src/main/java/elieomatuku/cineast_android/contents/ContentsActivity.kt
@@ -6,6 +6,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -61,6 +65,18 @@ class ContentsActivity : ComponentActivity() {
                         TopAppBar(
                             title = {
                                 Text(stringResource(id = screenNameRes))
+                            },
+                            navigationIcon = {
+                                IconButton(
+                                    onClick = {
+                                        onBackPressedDispatcher.onBackPressed()
+                                    }
+                                ) {
+                                    Icon(
+                                        imageVector = Icons.Filled.ArrowBack,
+                                        contentDescription = "Back"
+                                    )
+                                }
                             }
                         )
 
